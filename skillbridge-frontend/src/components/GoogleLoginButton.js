b7695c242e517d2d6c42b8b28e4b05588271cbb6
keerthana2624 <keerthanamummy2002@gmail.com>
2025-02-07 15:00:04 +0530

    created the register component

--- skillbridge-frontend/src/components/GoogleLoginButton.js
+++ skillbridge-frontend/src/components/GoogleLoginButton.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import { GoogleLogin } from 'react-oauth/google';
+function GoogleLoginButton({ onSuccess, onError }) {
+    return (
+      <GoogleLogin
+        onSuccess={onSuccess}
+        onError={onError}
+      />
+    );
+  }
+  
+  export default GoogleLoginButton;
